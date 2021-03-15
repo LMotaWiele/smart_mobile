9c9b8c3f0dc43449e18eaa0ebd92e2715d32ee59
Mendonça Menezes,Lucas P.L <l.mendoncamenezes@student.fontys.nl>
2021-03-15 17:07:44 +0100

    Nav bar partially implemented

--- app/src/main/java/com/example/hackme/ChatActivity.kt
+++ app/src/main/java/com/example/hackme/ChatActivity.kt
@@ -1,11 +1,33 @@
 package com.example.hackme
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackme.databinding.ActivityMainBinding
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_chat)
+
+        val MainA = Intent(this, MainActivity::class.java)
+        val MapA = Intent(this, MainMap::class.java)
+        val ChatA = Intent(this, ChatActivity::class.java)
+        val SettingsA = Intent(this, SettingsActivity::class.java)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_baseline_map_24 -> startActivity(MapA)
+                R.id.ic_baseline_comment_24 -> startActivity(ChatA)
+                R.id.ic_baseline_settings_24 -> startActivity(SettingsA)
+                R.id.ic_baseline_format_list_bulleted_24 -> startActivity(MainA)
+            }
+            true
+        }
     }
 }
